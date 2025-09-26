62ff223347dc71a134a953ecda8472cbbc308cc3
Fuzail Kazi <mohdfuzailkazi14@gmail.com>
2025-09-26 16:28:48 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <span className="bg-gradient-to-br from-green-300 to-green-700 bg-clip-text text-transparent">
+          ChatPRD
+        </span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
